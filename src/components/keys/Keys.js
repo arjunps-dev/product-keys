8b8cec5253f6d6da207e208328e01a7314e26d3a
arjunps-dev <arjunps179@gmail.com>
2022-12-03 07:48:48 +0530

    keys updated

--- src/components/keys/Keys.js
+++ src/components/keys/Keys.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 const products = [
     {id: 1,name: "Laptop", brand: "Dell", qty: 1},
     {id: 2,name: "Laptop", brand: "HP", qty: 1},
-    {id: 2,name: "Laptop", brand: "Asus", qty: 1}
+    {id: 3,name: "Laptop", brand: "Asus", qty: 1}
 ]
 const Keys = () => {
     const  [items, setItem] = useState(products);
+
     const changeQty = (id)=>{
         const newItem = items.map((item)=>
             item.id === id?{...item, qty: item.qty+1}: item
